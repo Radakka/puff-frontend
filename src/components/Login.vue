471c64d23acfb5b73a9c0238b14afa1498bacc2f
jaimegeh <jaimegeh@gmail.com>
2019-01-09 19:47:39 +0100

    Initial login form

---
+++ src/components/Login.vue
@@ -0,0 +1,47 @@
+<template>
+  <div class="login">
+    <form>
+      <input type="text" class="user" placeholder="username"></input>
+      <input type="password" class="password" placeholder="password"></input>
+      <div class="button">
+      	<a href="#">Submit</a>
+      </div>
+    </form>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'login',
+}
+</script>
+
+<style scoped lang="scss">
+  .login {
+  	border: 1px solid #444444;
+  	border-radius: 5px;
+  	background-color: #F8F8F8;
+  	width: 80%;
+  	margin: 0 auto;
+  	padding: 10px;
+  
+    input {
+      width: 90%;
+      margin: 10px;
+      padding: 10px;
+      border-radius: 5px;
+    }
+    
+    .button {
+    	margin: 10px;
+    	
+    	a {
+    	  background-color: #009933;
+    	  padding: 10px;
+    	  width: 90%;
+        color: #EEEEEE;
+        text-decoration: none;
+    	}
+    }
+  }
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,24 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1>Puff</h1>
+    <Login />
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import Login from '@/components/Login.vue'
 
 export default {
   name: 'home',
   components: {
-    HelloWorld
+    Login
   }
 }
 </script>
+
+<style scoped lang="scss">
+.home {
+  margin: 0 auto;
+}
+</style>
