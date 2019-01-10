ad06623f086928a20f3a6561e50795c83c0fe901
jaimegeh <jaimegeh@gmail.com>
2019-01-10 16:00:26 +0100

    Initial login request

---
+++ src/_services/UserService.js
@@ -0,0 +1,39 @@
+import config from './config'
+
+export default {
+	login,
+	logout
+};
+
+function login(username, password) {
+	const requestOptions = {
+		method: 'POST',
+		headers: {'Content-Type': 'application/json'},
+		body: JSON.stringify({ username, password})
+	};
+	
+	return fetch(`${config.apiUrl}/login`, requestOptions)
+	.then(handleResponse)
+	.then(response => {
+		if(response.token) {
+			alert(response.token);
+		}
+		return response;
+	});
+}
+
+function logout() {
+	alert('logout')
+}
+
+function handleResponse(response) {
+	return response.text().then(text => {
+		const data = text && JSON.parse(text);
+		if(!response.ok) {
+			const error = (data && data.message) || response.statusText;
+			return Promise.reject(error);
+		}
+		
+		return data;
+	});
+}
--- src/components/Login.vue
+++ src/components/Login.vue
@@ -1,29 +1,42 @@
 <template>
   <div class="login">
     <form>
-      <input type="text" class="user" placeholder="username"></input>
-      <input type="password" class="password" placeholder="password"></input>
+      <input v-model="username" type="text" class="user" placeholder="username" />
+      <input v-model="password" type="password" class="password" placeholder="password" />
       <div class="button">
-      	<a href="#">Submit</a>
+        <a v-on:click="submit">Submit</a>
       </div>
     </form>
   </div>
 </template>
 
 <script>
+import userService from '../_services/UserService';
+
 export default {
   name: 'login',
+  data: function () {
+    return {
+      username: '',
+      password: ''
+    }
+  },
+  methods: {
+    submit: function () {
+      userService.login(this.username, this.password)
+    },
+  }
 }
 </script>
 
 <style scoped lang="scss">
   .login {
-  	border: 1px solid #444444;
-  	border-radius: 5px;
-  	background-color: #F8F8F8;
-  	width: 80%;
-  	margin: 0 auto;
-  	padding: 10px;
+    border: 1px solid #444444;
+    border-radius: 5px;
+    background-color: #F8F8F8;
+    width: 80%;
+    margin: 0 auto;
+    padding: 10px;
   
     input {
       width: 90%;
@@ -33,15 +46,15 @@ export default {
     }
     
     .button {
-    	margin: 10px;
-    	
-    	a {
-    	  background-color: #009933;
-    	  padding: 10px;
-    	  width: 90%;
-        color: #EEEEEE;
-        text-decoration: none;
-    	}
+        margin: 10px;
+        
+        a {
+          background-color: #009933;
+          padding: 10px;
+          width: 90%;
+          color: #EEEEEE;
+          cursor: pointer;
+        }
     }
   }
 </style>
